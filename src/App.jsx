2fa5557dd78d2213ed46fc3f1f80acb3f2bca4b1
Abdul-Rafay-Munir <munirabdulrafay@gmail.com>
2025-11-03 10:47:29 +0500

    day 9

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,8 @@ function App() {
       .map(() => ({
         value: Math.ceil(Math.random() * 6),
         isHeld: false,
-        id:nanoid()
+        id:nanoid(),
+        className: 'w-[40px] h-[40px] sm:w-[50px] sm:h-[50px] border-none rounded-[10px] bg-white font-bold text-[1.4rem] sm:text-[1.75rem] shadow-[0_2px_2px_rgba(0,0,0,0.15)]'
       }))
   }
 
@@ -45,17 +46,17 @@ function App() {
   }
 
   const diceElements = dice.map(dieObj => (
-    <Dice key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} hold={()=>hold(dieObj.id)}/>))
+    <Dice key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} hold={()=>hold(dieObj.id)} className={dieObj.className}/>))
 
   return (
-    <main>
+    <main className='bg-[#F5F5F5] w-full max-w-[400px] rounded-[5px] flex flex-col justify-evenly items-center p-5'>
       {gameWon && <Confetti />}
-      <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className='dice-container'>
+      <h1 className="text-[28px] sm:text-[40px] m-0 font-bold">Tenzies</h1>
+      <p className="font-inter mt-0 text-center font-normal text-center">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className='grid grid-rows-2 grid-cols-5 gap-[10px] sm:gap-5 mb-[20px] sm:mb-10'>
         {diceElements}
       </div>
-      <button ref={buttonRef} onClick={rollDice} className='roll-dice'>
+      <button ref={buttonRef} onClick={rollDice} className='px-[18px] sm:px-[21px] py-[8px] sm:py-[6px] border-none bg-[#5035FF] text-white rounded-md text-base sm:text-lg'>
         {gameWon ? 'New Game' : 'Roll'}
       </button>
     </main>
--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -6,6 +6,7 @@ export default function Dice(props) {
         <button
             style={styles}
             onClick={props.hold}
+            className={props.className}
         >{props.value}</button>
     )
 }
